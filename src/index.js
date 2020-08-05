1f6836cce702d046a6d1d160b5613153af68c02e
Jun-cloud <kjy8827@gmail.com>
2020-08-05 19:55:40 +0900

    Set up redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
+import {createStore} from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = toDo => {
-    const li = document.createElement("li");
-    li.innerText = toDo;
-    ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+    console.log(action);
+    switch(action.type){
+        case ADD_TODO:
+            return []
+        case DELETE_TODO:
+            return []
+        default:
+            return state;
+    }
 }
+
+const store = createStore(reducer);
+
 
 const onSubmit = e => {
     e.preventDefault();
     const toDo = input.value;
     input.value = "";
-    createToDo(toDo);
+    // dispatch를 통해 데이터를 배열 형식으로 보낼 수 있음
+    store.dispatch({type: ADD_TODO, text: toDo});
 }
 
 form.addEventListener("submit", onSubmit);
