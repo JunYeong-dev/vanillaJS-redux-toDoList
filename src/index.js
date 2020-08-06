a8cbfb18ad8a5a0a709d34250777b242901d6d7c
Jun-cloud <kjy8827@gmail.com>
2020-08-06 20:30:02 +0900

    redux state - don't mutate state

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ const reducer = (state = [], action) => {
     console.log(action);
     switch(action.type){
         case ADD_TODO:
-            return []
+            /* 
+            기본적으로 redux의 state는 readonly -> action을 통해서만 변경함
+            ES6 spread
+            ...state : 간단히 말하면 배열을 풀어주는 것
+            [...state, {text: action.text}] : 기존 array인 state의 contents(내용) + 새로운 object -> 새로운 array 생성
+            기존의 state를 변경하지 않고, spread 연산자(...)를 사용하여 업데이트(새로 만듦)
+            */ 
+            return [...state, {text: action.text}]
         case DELETE_TODO:
             return []
         default:
@@ -21,6 +28,7 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = e => {
     e.preventDefault();
